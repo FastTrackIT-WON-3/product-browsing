b5966f929336eb4ea00fe56a281e04b042af6737
Florin Lazar <florin.lazar.it2014@gmail.com>
2022-05-09 19:41:35 +0300

    Scaffold UI and logic for categories

---
+++ ProductBrowsing/ProductBrowsing.Mvc/Controllers/CategoryController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProductBrowsing.Infrastructure;
+using ProductBrowsing.Infrastructure.Entities;
+
+namespace ProductBrowsing.Mvc.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly DatabaseContext _context;
+
+        public CategoryController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Category
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Categories.ToListAsync());
+        }
+
+        // GET: Category/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var categoryEntity = await _context.Categories
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (categoryEntity == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryEntity);
+        }
+
+        // GET: Category/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Category/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name")] CategoryEntity categoryEntity)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(categoryEntity);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categoryEntity);
+        }
+
+        // GET: Category/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var categoryEntity = await _context.Categories.FindAsync(id);
+            if (categoryEntity == null)
+            {
+                return NotFound();
+            }
+            return View(categoryEntity);
+        }
+
+        // POST: Category/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] CategoryEntity categoryEntity)
+        {
+            if (id != categoryEntity.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(categoryEntity);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CategoryEntityExists(categoryEntity.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categoryEntity);
+        }
+
+        // GET: Category/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var categoryEntity = await _context.Categories
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (categoryEntity == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryEntity);
+        }
+
+        // POST: Category/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var categoryEntity = await _context.Categories.FindAsync(id);
+            _context.Categories.Remove(categoryEntity);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CategoryEntityExists(int id)
+        {
+            return _context.Categories.Any(e => e.Id == id);
+        }
+    }
+}
