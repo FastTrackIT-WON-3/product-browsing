920acfaec200a9775d2cea040055dbf082cff249
Florin Lazar <florin.lazar.it2014@gmail.com>
2022-05-09 19:10:45 +0300

    create db context class

--- ProductBrowsing/ProductBrowsing.Core/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ProductBrowsing.Core
-{
-    public class Class1
-    {
-    }
-}
---
+++ ProductBrowsing/ProductBrowsing.Infrastructure/DatabaseContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProductBrowsing.Infrastructure
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options)
+        {
+        }
+    }
+}
