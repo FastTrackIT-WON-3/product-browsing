56b87c06e330abeb824db1089c30cf2f7eaac8a3
Florin Lazar <florin.lazar.it2014@gmail.com>
2022-05-09 20:59:43 +0300

    scaffold products UI

--- ProductBrowsing/ProductBrowsing.Mvc/Controllers/CategoryController.cs
+++ ProductBrowsing/ProductBrowsing.Mvc/Controllers/CategoryController.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
 using ProductBrowsing.Core.Abstractions.Services;
 using ProductBrowsing.Core.Models;
-using ProductBrowsing.Infrastructure;
-using ProductBrowsing.Infrastructure.Entities;
 using ProductBrowsing.Mvc.Extensions;
 using ProductBrowsing.Mvc.Models;
 
